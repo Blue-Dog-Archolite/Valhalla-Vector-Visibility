a4ba50a17e5b7a49429ccc48ac32c083a262d378
Blue-Dog-Archolite <Blue.Dog.Archolite@gmail.com>
2015-04-12 17:45:04 -0600

    Updates for VCR for ForecastIO

--- spec/lib/forecast_for_point_in_time_and_space_spec.rb
+++ spec/lib/forecast_for_point_in_time_and_space_spec.rb
@@ -16,5 +16,14 @@ describe ForecastForPointInTimeAndSpace, :vcr do
       )
     end
   end
+
+  it 'only calls to ForecastIO once per set of data' do
+    point = flight_path.first
+    key = "41.8781136,-87.6297982-1388559600"
+    REDIS.del(key)
+    expect(ForecastIO).to receive(:forecast).once.and_return({"key" => "value"})
+    subject.forecast_for(point[1], point[0])
+    subject.forecast_for(point[1], point[0])
+  end
 end
 
