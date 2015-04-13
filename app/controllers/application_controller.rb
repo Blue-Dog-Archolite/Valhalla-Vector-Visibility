000655387d1908d52326a8203b98cad543f59a66
Blue-Dog-Archolite <Blue.Dog.Archolite@gmail.com>
2015-04-13 17:48:30 -0600

    Devise Working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-#  respond_to :html, :json
+  respond_to :html, :json
+
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
 
   def render_model_error(model)
